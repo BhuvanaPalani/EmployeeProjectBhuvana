30287ee630e17f3a6a228f2a5827b7206a496396
bhuvana <bhuvi.pavi@gmail.com>
2022-12-23 22:07:15 +0100

    Update EmployeeController.java

--- Assignment1SpringBoot/Assignment1SpringBoot/src/main/java/com/example/Assignment1SpringBoot/EmployeeController.java
+++ Assignment1SpringBoot/Assignment1SpringBoot/src/main/java/com/example/Assignment1SpringBoot/EmployeeController.java
@@ -3,37 +3,48 @@ package com.example.Assignment1SpringBoot;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 
 
 
-@RestController
+@Controller
 @RequestMapping
 public class EmployeeController {
 	
-	//Employee employee = new Employee(123, "John", "Chief Engineer", 35000);
+	
 	
 	
 	@Autowired
 	EmployeeList employeeList;
 	
+	 
+	 @RequestMapping(value = "/company",  method = { RequestMethod.GET})
+	 @ResponseBody
+	 public String companyPage() {
+		 return "company.jsp";
+	 }
+	 
 	
 	
        @RequestMapping(value = "/employeebyname",  method = { RequestMethod.GET})
+       @ResponseBody
        public Employee getEmployee(String name) {
    		return employeeList.getEmployeeByName(name);
    	}
-	
+       
 	@RequestMapping(value = "/employees",  method = { RequestMethod.GET})
+	@ResponseBody
 	public Employee[] getEmployeeArray() {
 		return employeeList.getEmployeeAsArray();
 	}
 	
 	
 	@RequestMapping(value = "/addemployee",  method = { RequestMethod.POST})
+	@ResponseBody
 	public String addEmployee(Employee employee) { 
 		employeeList.add(employee);
 		return "Successfully Added!";
@@ -41,6 +52,7 @@ public class EmployeeController {
 	
 	
 	@RequestMapping(value = "/changeemployeedetail",  method = { RequestMethod.PUT})
+	@ResponseBody
 	public ResponseEntity<String> changeDesignation(String name, String newDesignation) {
 		boolean success = employeeList.changeDesignation(name, newDesignation);;
 		
@@ -51,6 +63,7 @@ public class EmployeeController {
 	}
 	
 	@RequestMapping(value = "/deleteemployee",  method = { RequestMethod.DELETE})
+	@ResponseBody
 	public ResponseEntity<String> deleteEmployee(String name) {
 		boolean success = employeeList.deleteEmployee(name);
 		if (success) {
